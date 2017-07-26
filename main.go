a2fa7c11185232ac8622f4e75de18a425469d1ac
Andrew G. Saushkin <asaushkin@gmail.com>
2017-07-26 10:49:55 +0300

    Add new command line option `-c`
    A new command line option `-c` can be used for assign the
    total brochure pages. In opposiotion of option `-l` which set
    the last page in the source book.

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 var (
 	fromPage     int
 	lastPage     int
+	countPage    int
 	bookName     string
 	brochureName string
 )
@@ -18,14 +19,17 @@ var (
 func init() {
 	flag.IntVar(&fromPage, "f", 1, "make brochure from page")
 	flag.IntVar(&lastPage, "l", 0, "make brochure last page")
+	flag.IntVar(&countPage, "c", 0, "count pages, this option conflicts with -l")
 	flag.StringVar(&bookName, "book", "origin.pdf", "origin pdf book")
 	flag.StringVar(&brochureName, "brochure", "print.ps", "target postscript brochure")
 }
 
 func main() {
 	flag.Parse()
-	if lastPage == 0 {
+	if lastPage == 0 && countPage == 0 {
 		lastPage = fromPage + 15
+	} else if lastPage == 0 && countPage != 0 {
+		lastPage = fromPage + countPage - 1
 	}
 
 	goexec.Execute("pdftops", "-f", strconv.Itoa(fromPage), "-l", strconv.Itoa(lastPage), "-paper", "A4", "-expand", bookName, "out.ps")
